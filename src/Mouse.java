cf91e0b491f1a31c1c171631391ec6196269cdb9
BT_4N2_01 <artyoms5@nycstudents.net>
2024-03-04 13:45:48 -0500

    i did dogwater. boooooooo. Haroon is dummy dum dum. Ricky is stealing our coooooode. i wanna sleep. Haroon is crossing the bordeeeeeer. goo goo gaa gaaa. I dont think harron is mentally capable of being sane. I'm really bored, please ignore thisssss. Ricky is playing pokemooooooon. Oh yea and i added more codeeeee. waaaaaaaaaaaa.

--- src/Mouse.java
+++ src/Mouse.java
@@ -12,32 +12,39 @@ public class Mouse {
     public void pathfinder(){
          int[] start = {0,0};
          int[] end = {maze.length - 1, maze[0].length - 1};
+         road.add(start);
+         int[] temp;
+         while(road.get(road.size()-1)[0] == end[0] && road.get(road.size()-1)[1] == end[1]){
+             temp = direction(road.get(road.size()-1));
+             if(temp[1] != 0){
+
+             }
 
 
 
+         }
     }
 
-    public int direction(int[] location){
-        int theWay = 6;
+    public int[] direction(int[] location){
+        int[] theWay = {4,0};
         if(location[1] != 0 && maze[location[0]][location[1] - 1] == "."){
-            theWay = 1;
+            theWay[0] = 1;
+            theWay[1]++;
+            +
         }
         if(location[0] != maze.length && maze[location[0] + 1][location[1]] == "."){
-            if(2 < theWay){
-                theWay = 2;
+            if(2 < theWay[0]){
+                theWay[0] = 2;
+                theWay[1]++;
             }
         }
         if(location[1] != maze[0].length && maze[location[0]][location[1] + 1] == "."){
-            if(3 < theWay){
-                theWay = 3;
+            if(3 < theWay[0]){
+                theWay[0] = 3;
+                theWay[1]++;
             }
         }
-        if(location[0] != 0 && maze[location[0] + 1][location[1]] == "."){
-            if(4 < theWay){
-                theWay = 4;
-            }
-        }
-
+        return theWay;
     }
 
 
