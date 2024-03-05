5f4d7d88bd69ba6cda60656362045c31b5728bdb
BT_4N2_01 <artyoms5@nycstudents.net>
2024-03-05 13:45:37 -0500

    ROOTBEEEEEEEEAAAR!!! i made the code make sense more. i started with part 2 and forgot about part 1. WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        getMaze("Maze");
+        Mouse maze1 = new Mouse(getMaze("Maze"));
 
     }
 
--- src/Mouse.java
+++ src/Mouse.java
@@ -1,24 +1,26 @@
 import java.util.ArrayList;
 
 public class Mouse {
-    private String[][] maze;
+    private final String[][] maze;
     private String[][] path;
-    ArrayList<int[]> road = new ArrayList<int[]>();
+    ArrayList<int[]> cords = new ArrayList<int[]>();
     public Mouse(String[][] maze){
         this.maze = maze;
         path = new String[this.maze.length][this.maze[0].length];
+        for(int i = 0; i < this.maze.length; i++){
+            for(int j = 0; j < this.maze[0].length; j++){
+                path[i][j] = this.maze[i][j];
+            }
+        }
     }
 
     public void pathfinder(){
-         int[] start = {0,0};
+         int[] start = {0,0,0};
          int[] end = {maze.length - 1, maze[0].length - 1};
-         road.add(start);
-         int[] temp;
-         while(road.get(road.size()-1)[0] == end[0] && road.get(road.size()-1)[1] == end[1]){
-             temp = direction(road.get(road.size()-1));
-             if(temp[1] != 0){
+         cords.add(start);
+         int[][] Haroon = new int[500][2];
+         while(cords.get(cords.size()-1)[0] == end[0] && cords.get(cords.size()-1)[1] == end[1]){
 
-             }
 
 
 
@@ -26,24 +28,30 @@ public class Mouse {
     }
 
     public int[] direction(int[] location){
-        int[] theWay = {4,0};
-        if(location[1] != 0 && maze[location[0]][location[1] - 1] == "."){
+        int[] theWay = {5,0};
+        if(location[1] != 0 && maze[location[0]][location[1] - 1] == "."){ // left
             theWay[0] = 1;
             theWay[1]++;
-            +
         }
-        if(location[0] != maze.length && maze[location[0] + 1][location[1]] == "."){
+        if(location[0] != maze.length && maze[location[0] - 1][location[1]] == "."){ // down
             if(2 < theWay[0]){
                 theWay[0] = 2;
                 theWay[1]++;
             }
         }
-        if(location[1] != maze[0].length && maze[location[0]][location[1] + 1] == "."){
+        if(location[1] != maze[0].length && maze[location[0]][location[1] + 1] == "."){  // right
             if(3 < theWay[0]){
                 theWay[0] = 3;
                 theWay[1]++;
             }
         }
+        if(location[0] != maze[0].length && maze[location[0] + 1][location[1]] == "."){  // up
+            if(4 < theWay[0]){
+                theWay[0] = 4;
+                theWay[1]++;
+            }
+        }
+        cords.get(cords.size() - 1)[2] = theWay[1];
         return theWay;
     }
 
